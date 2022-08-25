d2a038e063766abebabb7429ed7711cbe4f21b18
Loc Phan <locphan@Locs-MacBook-Air.local>
2022-08-26 08:50:28 +0900

    fix text of header

--- resources/views/admin/blogs/blogs_all.blade.php
+++ resources/views/admin/blogs/blogs_all.blade.php
@@ -22,7 +22,7 @@
             <div class="card">
                 <div class="card-body">
 
-                    <h4 class="card-title">Blogs All Data</h4>
+                    <h4 class="card-title">Blogs All</h4>
 
 
                     <table id="datatable" class="table table-bordered dt-responsive nowrap" style="border-collapse: collapse; border-spacing: 0; width: 100%;">
