8202f306675bb9c9a4f6dd4b16d7be05a5c0a3f6
phandinhlocbk <phandinhlocbk@gmail.com>
2023-07-01 22:40:41 +0900

    add

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -44,16 +44,8 @@ class AdminController extends Controller
     public function EditProfile()
     {
         $id = Auth::user()->id;
-        $adminData = User::find($id);
-        return view('admin.admin_profile_view', compact('adminData'));
-    }
-
-    public function EditProfile()
-    {
-        $id = Auth::user()->id;
         dd($id);
         #test
->>>>>>> 0d8f94b (test)
         $editData = User::find($id);
         #test
         return view('admin.admin_profile_edit', compact('editData'));
